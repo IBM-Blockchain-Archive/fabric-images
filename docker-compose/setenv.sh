1a111fbe42d133770c1d67df99e95b7b1de55e9a
Gari Singh <gari.r.singh@gmail.com>
2016-10-14 15:06:55 -0400

    Update setenv.sh - correct image name for ARCH=ppc64le

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -6,8 +6,8 @@ elif [ $(uname -m) == "s390x" ]; then
     export ARCH_MEMSVC="fabric-membersrvc:s390x-0.6.0-preview"
     export ARCH_PEER="ufabric-peer:s390x-0.6.0-preview"
 elif [ $(uname -m) == "ppc64le" ]; then
-    export ARCH_MEMSVC="fabric-membersrvc:s390x-0.6.0-preview"
-    export ARCH_PEER="fabric-peer:s390x-0.6.0-preview"
+    export ARCH_MEMSVC="fabric-membersrvc:ppc64le-0.6.0-preview"
+    export ARCH_PEER="fabric-peer:ppc64le-0.6.0-preview"
 else
     export ARCH_MEMSVC="unknownarch"
     export ARCH_PEER="unknownarch"
