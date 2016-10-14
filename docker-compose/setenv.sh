4b9957491c8f1ed3c4d9f5d6d0a4f8b699bfbb9c
Latitia M Haskins <latitia.haskins@gmail.com>
2016-10-14 17:44:59 -0400

    Update to allow for building of the base image

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 if [ $(uname -m) == "x86_64" ] || [ $(uname -m) == "x86_32" ]; then
-    export ARCH_MEMSVC="fabric-membersrvc:x86_64-0.6.0-preview"
-    export ARCH_PEER="fabric-peer:x86_64-0.6.0-preview"
+    export ARCH_TAG="x86_64-0.6.0-preview"
 elif [ $(uname -m) == "s390x" ]; then
-    export ARCH_MEMSVC="fabric-membersrvc:s390x-0.6.0-preview"
-    export ARCH_PEER="ufabric-peer:s390x-0.6.0-preview"
+    export ARCH_TAG="s390x-0.6.0-preview"
 elif [ $(uname -m) == "ppc64le" ]; then
-    export ARCH_MEMSVC="fabric-membersrvc:ppc64le-0.6.0-preview"
-    export ARCH_PEER="fabric-peer:ppc64le-0.6.0-preview"
+    export ARCH_TAG="ppc64le-0.6.0-preview"
 else
-    export ARCH_MEMSVC="unknownarch"
-    export ARCH_PEER="unknownarch"
+    export ARCH_TAG="unknownarch"
 fi
