41e0498713a7adef73bca2ee2992ba3ed53114a0
Krishna Harsha Voora <harsha544@users.noreply.github.com>
2016-10-15 01:03:58 +0530

    Mention of additional character
    "u" was mentioned in docker image of ARCH_PEER for s390x 
    export ARCH_PEER="ufabric-peer:s390x-0.6.0-preview"
    
    Instead Should be 
    export ARCH_PEER="fabric-peer:s390x-0.6.0-preview"

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -4,7 +4,7 @@ if [ $(uname -m) == "x86_64" ] || [ $(uname -m) == "x86_32" ]; then
     export ARCH_PEER="fabric-peer:x86_64-0.6.0-preview"
 elif [ $(uname -m) == "s390x" ]; then
     export ARCH_MEMSVC="fabric-membersrvc:s390x-0.6.0-preview"
-    export ARCH_PEER="ufabric-peer:s390x-0.6.0-preview"
+    export ARCH_PEER="fabric-peer:s390x-0.6.0-preview"
 elif [ $(uname -m) == "ppc64le" ]; then
     export ARCH_MEMSVC="fabric-membersrvc:ppc64le-0.6.0-preview"
     export ARCH_PEER="fabric-peer:ppc64le-0.6.0-preview"
