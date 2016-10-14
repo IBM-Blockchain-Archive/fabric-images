ab5d6b3c6c5fe08e9ac46f0ca739105fc05eaf6f
Latitia M Haskins <latitia.haskins@gmail.com>
2016-10-14 17:57:59 -0400

    Update with 0.6.1 instead of 0.6.0

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $(uname -m) == "x86_64" ] || [ $(uname -m) == "x86_32" ]; then
-    export ARCH_TAG="x86_64-0.6.0-preview"
+    export ARCH_TAG="x86_64-0.6.1-preview"
 elif [ $(uname -m) == "s390x" ]; then
-    export ARCH_TAG="s390x-0.6.0-preview"
+    export ARCH_TAG="s390x-0.6.1-preview"
 elif [ $(uname -m) == "ppc64le" ]; then
-    export ARCH_TAG="ppc64le-0.6.0-preview"
+    export ARCH_TAG="ppc64le-0.6.1-preview"
 else
     export ARCH_TAG="unknownarch"
 fi
