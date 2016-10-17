92fa3ca1259a06c591eccbda1631495b11d149cc
Latitia M Haskins <latitia.haskins@gmail.com>
2016-10-17 16:56:41 -0400

    Adding the use of the BASE_TAG

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-if [ $(uname -m) == "x86_64" ] || [ $(uname -m) == "x86_32" ]; then
+if [ $(uname -m) == "x86_64" ]; then
     export ARCH_TAG="x86_64-0.6.1-preview"
+    export BASE_TAG="x86_64-0.0.11"
 elif [ $(uname -m) == "s390x" ]; then
     export ARCH_TAG="s390x-0.6.1-preview"
+    export BASE_TAG="s390x-0.0.11"
 elif [ $(uname -m) == "ppc64le" ]; then
     export ARCH_TAG="ppc64le-0.6.1-preview"
+    export BASE_TAG="ppc64le-0.0.11"
 else
     export ARCH_TAG="unknownarch"
+    export BASE_TAG="unknownarch"
+    echo "WARNING:" $(uname -m) "is an unsupported architecture"
 fi
