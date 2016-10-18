3d71cb14219157a1fd443e30f7a81c466b56a30d
Latitia M. Haskins <latitia.haskins@gmail.com>
2016-10-18 16:28:27 +0000

    We need the baseimage in order for deploys to work correctly

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-if [ $(uname -m) == "x86_64" ]; then
-    export ARCH_TAG="x86_64-0.6.1-preview"
-    export BASE_TAG="x86_64-0.0.11"
-elif [ $(uname -m) == "s390x" ]; then
-    export ARCH_TAG="s390x-0.6.1-preview"
-    export BASE_TAG="s390x-0.0.11"
-elif [ $(uname -m) == "ppc64le" ]; then
-    export ARCH_TAG="ppc64le-0.6.1-preview"
-    export BASE_TAG="ppc64le-0.0.11"
-else
-    export ARCH_TAG="unknownarch"
-    export BASE_TAG="unknownarch"
-    echo "WARNING:" $(uname -m) "is an unsupported architecture"
-fi
+
+arch=`uname -m`
+
+case $arch in
+"x86_64")
+      export ARCH_TAG="x86_64-0.6.1-preview"
+  ;;
+"s390x")
+      export ARCH_TAG="s390x-0.6.1-preview"
+  ;;
+"ppc64le")
+      export ARCH_TAG="ppc64le-0.6.1-preview"
+  ;;
+*)
+  echo "No Architectural Images Available for Architecture: $arch - Please call ibm service"
+  return
+  ;;
+esac
+
+cat baseimage/Dockerfile.in | sed -e "s/_ARCH_TAG_/$ARCH_TAG/g" > baseimage/Dockerfile
