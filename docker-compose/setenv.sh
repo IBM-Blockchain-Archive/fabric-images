49f29d47964b946207aa3f262c0663e71b0a977a
bmos299 <barry_moz@yahoo.com>
2016-10-18 09:32:52 -0400

    overhaul of the script

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
-if [ $(uname -m) == "x86_64" ]; then
-    export ARCH_TAG="x86_64-0.6.1-preview"
-    export BASE_TAG="x86_64-0.0.11"
-elif [ $(uname -m) == "s390x" ]; then
-    export ARCH_TAG="s390x-0.6.1-preview"
-    export BASE_TAG="s390x-0.0.11"
-elif [ $(uname -m) == "ppc64le" ]; then
-    export ARCH_TAG="ppc64le-0.6.1-preview"
-    export BASE_TAG="ppc64le-0.0.11"
-else
-    export ARCH_TAG="unknownarch"
-    export BASE_TAG="unknownarch"
-    echo "WARNING:" $(uname -m) "is an unsupported architecture"
+
+arch=`uname -m`
+
+case $arch in
+"x86_64")
+      export ARCH_TAG="x86_64-0.6.1-preview"
+  ;;
+"s390x")
+      export ARCH_TAG="s390x-0.6.1-preview"
+  ;;
+"ppc64le")
+      export ARCH_TAG="ppc64le-0.6.1-preview"       
+  ;;
+*)
+  echo "No Architectural Images Available for Architecture: $arch - Please call ibm service"
+  return 
+  ;;
+esac
+
+docker pull hyperledger/fabric-peer:${ARCH_TAG} 
+if [ $? -ne 0 ]; then
+   echo "docker pull failed to peer image"
+   return 
 fi
+docker tag hyperledger/fabric-peer:${ARCH_TAG} hyperledger/fabric-baseimage:latest
+
+if [ $? -ne 0 ]; then
+   echo "docker tag failed"
+   return
+fi
+
